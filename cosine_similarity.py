1e11afb49965afdfdb7793466eeaee8e6f2cc393
Uichi <ykonishi@protonmail.com>
2018-04-08 01:29:56 +0900

    Refactor the cosine similarity code.

--- cosine_similarity.py
+++ cosine_similarity.py
@@ -1,7 +1,8 @@
 # -*- coding:utf-8 -*-
 import pandas as pd
 import numpy as np
-from math import sqrt
+from collections import OrderedDict
+# from math import sqrt
 
 csv = input("読み込むｃｓｖファイルを入力してください。 : ")
 table = pd.read_csv(csv, encoding='utf-8')
@@ -10,7 +11,7 @@ items = []
 for i in range(1, len(table.columns)):
     items.append(table.columns[i:i+1][0])
 
-taste_dict = {}
+taste_dict = OrderedDict()
 for item in items:
     item_taste = []
     for i in range(len(items)):
@@ -20,36 +21,31 @@ for item in items:
 # taste = pd.Series(taste_dict)
 
 # 趣向データのベクトル化
-everything_vector = {}
+all_vector = OrderedDict()
 for i in range(len(taste_dict)):
     taste_vector = []
-    # for color in taste.index:
-    for color in taste_dict.keys():
+    for key in taste_dict.keys():
         vector = 0
-        # for j in range(len(taste[color])):
-        for j in range(len(taste_dict[color])):
-            # if taste[i][j] == taste[color][j] and taste[i][j] == 1:
-            if taste_dict[color][j] == taste_dict[color][j] and taste_dict[i][j] == 1:
+        for j in range(len(taste_dict[key])):
+            if taste_dict[list(taste_dict.keys())[i]][j] == taste_dict[key][j] and taste_dict[list(taste_dict.keys())[i]][j] == 1:
                 vector += 1
         taste_vector.append(vector)
-    all_vector[color] = taste_vector
-# all_vector = pd.Series(everything_vector)
+    all_vector[list(taste_dict.keys())[i]] = taste_vector
 df_vector = pd.DataFrame(all_vector, index=all_vector.keys())
 df_vector.to_csv('vector_' + csv, encoding='utf-8')
 
-exit()
 # 趣向ベクトルからコサイン類似度を算出
-cosine_similarity = {}
-for i in range(len(all_vector)):
+cosine_similarity = OrderedDict()
+for i, key in enumerate(all_vector.keys()):
     vector = []
-    q_vector = np.linalg.norm(all_vector[i])
+    q_vector = np.linalg.norm(all_vector[key])
     for nan_times in range(i):
         vector.append(np.nan)
     for j in range(i, len(all_vector)):
-        r_vector = np.linalg.norm(all_vector[j])
-        qr_vector = np.dot(all_vector[i], all_vector[j])
+        r_vector = np.linalg.norm(list(all_vector.values())[j])
+        qr_vector = np.dot(all_vector[key], list(all_vector.values())[j])
         vector.append(round(qr_vector/(q_vector*r_vector), 2))
-    cosine_similarity[all_vector.index[i]] = vector
+    cosine_similarity[key] = vector
 
 df = pd.DataFrame(cosine_similarity)
 df = df.T
