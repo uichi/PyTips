2c0e88b2a874eb5d3445e53c2072353667c486a1
uichi <ykonishi@protonmail.com>
2018-04-20 23:53:51 +0900

    How to use lamda.

--- lambda.py
+++ lambda.py
@@ -1,3 +1,7 @@
 #lambda使用例
 add_consumption = lambda amount, tax : amount + amount*tax/100
 print('Consumption tax : ', add_consumption(100, 8))
+
+num_list = list(range(0,99))
+square_list = list(map(lambda x : x**2, num_list))
+print(square_list)
